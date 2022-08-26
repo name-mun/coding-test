e07610051789f024bbac7cb7f380d1974119bd41
mun <dev.ceomun@gmail.com>
2022-08-27 02:11:51 +0900

    다이나믹프로그래밍-2193_이친수 방법2 글 참고

--- 알고리즘/다이나믹프로그래밍/2193-이친수.js
+++ 알고리즘/다이나믹프로그래밍/2193-이친수.js
@@ -1,18 +1,10 @@
 const input = require("fs").readFileSync("example.txt").toString().trim();
-
 n = Number(input);
 
-let arr = [
-  [0, 0],
-  [1, 0],
-  [1, 0],
-];
+let arr = [0, 1, 1];
 
 for (let i = 3; i <= n; i++) {
-  arr[i] = [
-    BigInt(arr[i - 1][0]) + BigInt(arr[i - 1][1]),
-    BigInt(arr[i - 1][0]),
-  ];
+  arr[i] = BigInt(arr[i - 1]) + BigInt(arr[i - 2]);
 }
 
-console.log((BigInt(arr[n][0]) + BigInt(arr[n][1])).toString());
+console.log(BigInt(arr[n]).toString());
